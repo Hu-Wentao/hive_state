bd1a3bc7fe15ac0fa714751ec5b758a78c5208f5
WyattHu(胡文涛) <wyatt.hu@hisunglobal.com>
2025-05-13 09:57:56 +0800

    feat loggable add log method

--- lib/src/mixin/loggable.dart
+++ lib/src/mixin/loggable.dart
@@ -1,20 +1,35 @@
-import 'dart:developer';
+import 'dart:async';
+import 'dart:developer' as dev;
 
 import 'package:flutter/foundation.dart';
 import 'package:hive_state/src/base.dart';
 
 /// 使用[log] 打印异常信息
 mixin LoggableMx<T> on BaseHiveState<T> {
+  log(
+    String message, {
+    DateTime? time,
+    int? sequenceNumber,
+    int level = 0,
+    String? name, // null will use 'stateKey'
+    Zone? zone,
+    Object? error,
+    StackTrace? stackTrace,
+  }) {
+    if (!kDebugMode) return;
+    dev.log(message,
+        time: time,
+        sequenceNumber: sequenceNumber,
+        level: level,
+        name: name ?? stateKey,
+        zone: zone,
+        error: error,
+        stackTrace: stackTrace);
+  }
+
   @override
   BaseHiveState<T> putError(Object e, [StackTrace? s]) {
-    if (kDebugMode) {
-      log(
-        '${valueToString(valueOrNull)}\n'
-        '$e\n'
-        '$s',
-        name: stateKey,
-      );
-    }
+    log('${valueToString(valueOrNull)}\n $e\n $s');
     return super.putError(e);
   }
 
