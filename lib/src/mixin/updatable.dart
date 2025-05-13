a519e905375ced11d540511fe36168426f997311
WyattHu(胡文涛) <wyatt.hu@hisunglobal.com>
2025-05-13 11:20:03 +0800

    feat TryUpdatableMx add 'onError' callback

--- lib/src/mixin/updatable.dart
+++ lib/src/mixin/updatable.dart
@@ -7,23 +7,23 @@ import 'package:hive_state/src/mixin/loggable.dart';
 mixin TryUpdatableMx<T> on BaseHiveState<T>, LoggableMx<T> {
   /// 执行一个异步操作, 并更新状态
   /// 不建议对本方法进行二次包装, 因此返回值强制为 void
-  Future<void> update(FutureOr<T> Function(T old) update) async {
-    try {
-      final data = await update(value);
-      put(data);
-    } catch (e, s) {
-      putError(e, s);
-    }
-  }
+  Future<void> update(
+    FutureOr<T> Function(T old) update, {
+    Function(Object e, StackTrace s)? onError,
+  }) =>
+      updateOrNull((old) => update(old as T), onError: onError);
 
-  /// 'use tryUpdate: 不建议将初始值设为null,带来额外的null检查步骤'
-  /// 本函数建议只用于 init数据场景下
-  Future<void> updateOrNull(FutureOr<T> Function(T? old) update) async {
+  /// if State init value is `null`, you can use [updateOrNull]
+  Future<void> updateOrNull(
+    FutureOr<T> Function(T? old) update, {
+    Function(Object e, StackTrace s)? onError,
+  }) async {
     try {
       final data = await update(valueOrNull);
       put(data);
     } catch (e, s) {
-      putError(e, s);
+      onError?.call(e, s);
+      if (onError == null) putError(e, s);
     }
   }
 }
